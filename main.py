d2615ee204ad6278b8b443c6aeb1ea2f2a6e8c5e
BalashovDV <korochun@yandex.ru>
2024-07-19 06:42:26 +0300

    1. Удалены неиспользующиеся пакеты. 2. Реализовано отображение температуры на два дня. 3. Немного переделана страница, отображение разбито по дням. Пока сегодня и завтра.

--- main.py
+++ main.py
@@ -1,68 +1,56 @@
-import openmeteo_requests
-from openmeteo_sdk.Variable import Variable
-
-import json
-
-import requests, sys
-
+from flask import Flask, render_template, request
+import requests
 from geopy.geocoders import Nominatim
 
-def getGeoData():
-    # response = requests.get('http://ip-api.com/json?lang=ru')
-    # data = response.json()
-    # return data
+
+def get_geo_data(city):
     geolocator = Nominatim(user_agent="MeteoData")
-    location = geolocator.geocode('Ковров')
+    location = geolocator.geocode(city)
     return location
 
 
-def getWeather():
-    geo = getGeoData()
-    print(geo)
-    print(geo.latitude, geo.longitude)
-    return
+app = Flask(__name__)
 
-    # params = {
-    #     'units': 'metric',
-    #     'lang': 'ru'
-    # }
 
-    om = openmeteo_requests.Client()
+@app.route('/', methods=['POST', 'GET'])
+def weather():
+    if request.method == 'POST':
+        city_name = request.form['city']
+    else:
+        city_name = 'Москва'
+    
+    geo = get_geo_data(city=city_name)
     params = {
-        "latitude": geo['lat'],
-        "longitude": geo['lon'],
-        "hourly": ["temperature_2m", "precipitation", "wind_speed_10m"],
-        "current": ["temperature_2m", "relative_humidity_2m"]
+        "latitude": geo.latitude,
+        "longitude": geo.longitude,
+        "daily": "temperature_2m_min,temperature_2m_max,precipitation_sum",
+        "timezone": "Europe/Moscow"
     }
-
-    responses = om.weather_api("https://api.open-meteo.com/v1/forecast",
-                               params=params)
-    response = responses[0]
-
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-
-    # Current values
-    current = response.Current()
-    current_variables = list(map(lambda i: current.Variables(i),
-                                 range(0, current.VariablesLength())))
-    current_temperature_2m = next(filter(
-        lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2,
-        current_variables))
-    current_relative_humidity_2m = next(filter(lambda
-                                                   x: x.Variable() == Variable.relative_humidity and x.Altitude() == 2,
-                                               current_variables))
-
-    print(f"Current time {current.Time()}")
-    print(f"Current temperature_2m {current_temperature_2m.Value()}")
-    print(
-        f"Current relative_humidity_2m {current_relative_humidity_2m.Value()}")
-
-def main():
-    getWeather()
+    response = requests.get("https://api.open-meteo.com/v1/forecast",
+                            params=params)
+    
+    if response.status_code == 200:
+        response_json = response.json()
+        data = {
+            "cityname": city_name,
+            "temp_min": str(response_json['daily']['temperature_2m_min'][0]),
+            "temp_max": str(response_json['daily']['temperature_2m_max'][0]),
+        }
+        # Поскольку индекс 0 представляет собой данные на текущий день,
+        # индекс 1 будет представлять данные на завтра
+        tomorrow_temp_min = response_json['daily']['temperature_2m_min'][1]
+        tomorrow_temp_max = response_json['daily']['temperature_2m_max'][1]
+        tomorrow_precipitation = response_json['daily']['precipitation_sum'][1]
+        
+        print(f"Прогноз погоды в {city_name} на завтра:")
+        print(f"Минимальная температура: {tomorrow_temp_min}°C")
+        print(f"Максимальная температура: {tomorrow_temp_max}°C")
+        print(f"Ожидаемое количество осадков: {tomorrow_precipitation} мм")
+        return render_template('index.html', data=data)
+    else:
+        print(f"Ошибка {response.status_code}: {response.text}")
+        return render_template('/')
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
